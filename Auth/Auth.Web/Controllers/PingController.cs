7216fef6d0095bfe5d14bdfa0fc7d259913a9868
Iulian Holonca <hoi@officetimeline.com>
2025-09-26 20:59:04 +0300

    Docker support

---
+++ Auth/Auth.Web/Controllers/PingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Auth.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok("pong");
+        }
+    }
+}
