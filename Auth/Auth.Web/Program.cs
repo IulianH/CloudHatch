61010dd5d091daedd13e356aeca9b4fd52181ded
Iulian Holonca <hoi@officetimeline.com>
2025-09-29 20:38:15 +0300

    Working version of login

--- Auth/Auth.Web/Program.cs
+++ Auth/Auth.Web/Program.cs
@@ -3,7 +3,6 @@ using Auth.App.Interface.RefreshToken;
 using Auth.Infra;
 using Auth.Web.Middleware;
 using Microsoft.AspNetCore.HttpOverrides;
-using Microsoft.AspNetCore.OpenApi;
 using Scalar.AspNetCore;
 
 
@@ -19,21 +18,6 @@ builder.Services.AddOpenApi(options => options.AddScalarTransformers());
 builder.Services.RegisterApplication(builder.Configuration);
 
 builder.Services.RegisterInfrastructure(builder.Configuration);
-
-// Add CORS for the development environment if enabled in configuration
-var corsEnabled = builder.Environment.IsDevelopment() && builder.Configuration.GetValue<bool>("EnableCors");
-if (corsEnabled)
-{
-    builder.Services.AddCors(options =>
-    {
-        options.AddPolicy("DevelopmentPolicy", policy =>
-        {
-            policy.AllowAnyOrigin()
-                  .AllowAnyMethod()
-                  .AllowAnyHeader();
-        });
-    });
-}
 
 var app = builder.Build();
 
@@ -56,16 +40,8 @@ if (app.Environment.IsDevelopment())
     
     // Add Scalar UI for API documentation
     app.MapScalarApiReference();
-    
-    // Enable CORS for development if enabled in configuration
-    if (corsEnabled)
-    {
-        app.UseCors("DevelopmentPolicy");
-    }
 }
 
-// No need when running in a container
-//app.UseHttpsRedirection();
 
 // Add global exception handling for Token library exceptions
 app.UseMiddleware<UnhandledExceptionMiddleware>();
